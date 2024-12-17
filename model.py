18ff5e6288fd1c3905f5374ebd96d904b75211fd
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-17 20:53:00 +0900

    creating numpy arrays for the model to be trained on

--- model.py
+++ model.py
@@ -1,1 +1,17 @@
 from sklearn.svm import LinearSVC
+import numpy as np 
+import cv2 as cv
+import PIL
+
+class Model:
+
+    def __init__(self):
+        self.model = LinearSVC()
+
+    def train_model(self, counters):
+        img_list = np.array([])
+        class_list = np.array([])
+
+        for i in range(1, counters[0]):
+            img = cv.imread(f'1/frame{i}.jpg')[:,:,0]
+            img = img.reshape(16800)
