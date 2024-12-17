9babc4863ef2d29bfe88d877223f08118701c041
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-17 21:00:46 +0900

    predict function

--- model.py
+++ model.py
@@ -15,3 +15,22 @@ class Model:
         for i in range(1, counters[0]):
             img = cv.imread(f'1/frame{i}.jpg')[:,:,0]
             img = img.reshape(16800)
+            img_list = np.append(img_list, [img])
+            class_list = np.append(class_list, 1)
+
+        for i in range(1, counters[1]):
+            img = cv.imread(f'2/frame{i}.jpg')[:,:,0]
+            img = img.reshape(16800)
+            img_list = np.append(img_list, [img])
+            class_list = np.append(class_list, 2)
+
+        img_list = img_list.reshape(counters[0] - 1 + counters[1] - 1, 16800)
+        self.model.fit(img_list, class_list)
+        print("Model Successfully Trained!")
+
+    def predict(self, frame):
+        frame = frame[1]
+        cv.imwrite('frame.jpg', cv.cvtColor(cv.COLOR_RGB2GRAY))
+        img = PIL.Image.open('frame.jpg')
+        img.thumbnail((150, 150), PIL.Image.ANTIALIAS)
+        img.save(f'frame.jpg')
